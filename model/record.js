6ee7350bfe5a9f993add49f4b78c5223a8be6ba4
JasonChou <swchou524@gmail.com>
2022-06-23 20:21:28 +0800

    feat: add category and record model

---
+++ model/record.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const recordSchema = new Schema({
+  id: {
+    type: Number,
+    require: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    require: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+})
+
+module.exports = mongoose.model('Record', recordSchema)
