6548b093c7e85a3c1cc8cbb861ac2a8344b27af3
JasonChou <swchou524@gmail.com>
2022-06-24 00:36:05 +0800

    feat: add body-parser

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const routes = require('./routes')
 const app = express()
 
@@ -8,6 +9,8 @@ require('./config/mongoose')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(routes)
 
