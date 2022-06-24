717d9fa37414e64a2c59fb8eb9735ed4ebcb56fb
JasonChou <swchou524@gmail.com>
2022-06-25 01:29:13 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const routes = require('./routes')
 const methodOverride = require('method-override')
+const usePassport = require('./config/passport')
 const app = express()
 
 require('dotenv').config()
@@ -32,6 +33,8 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(methodOverride('_method'))
+
+usePassport(app)
 
 app.use(routes)
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,14 +1,23 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../../models/user')
+const passport = require('passport')
 
 router.get('/login', (req, res) => {
   res.render('login')
 })
-router.post('/login', (req, res) => {})
+router.post(
+  '/login',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/users/login',
+  })
+)
+
 router.get('/register', (req, res) => {
   res.render('register')
 })
+
 router.post('/register', (req, res) => {
   // 取得註冊表單參數
   const { name, email, password, confirmPassword } = req.body
@@ -37,4 +46,5 @@ router.post('/register', (req, res) => {
     })
     .catch((err) => console.log(err))
 })
+
 module.exports = router
