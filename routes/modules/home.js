e93eac17340f2d180acd2d2ab54c42afe845a4fe
JasonChou <swchou524@gmail.com>
2022-06-24 15:04:42 +0800

    refactor: home route

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,15 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 const Record = require('../../models/record')
-const Category = require('../../models/category')
-const { render } = require('express/lib/response')
 
 router.get('/', (req, res) => {
   Record.find()
     .populate('categoryId')
     .lean()
     .then((records) => {
-      console.log(records)
       res.render('index', { records })
     })
     .catch((err) => console.error(err))
