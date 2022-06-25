0e1a59ed32a0dc099c8cffec01f8b1437482ab60
JasonChou <swchou524@gmail.com>
2022-06-26 00:26:57 +0800

    feat: add totalAmount and categorySelector

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,17 +2,56 @@ const express = require('express')
 const router = express.Router()
 
 const Record = require('../../models/record')
+const Category = require('../../models/category')
 
 router.get('/', (req, res) => {
   const userId = req.user._id
-  Record.find({ userId })
-    .populate('categoryId')
+  const categorySelectId = req.query.category
+  let categorySelectName = '選擇類別'
+
+  Category.find()
     .lean()
-    .then((records) => {
-      records.forEach((record) => {
-        record.date = record.date.toISOString().slice(0, 10)
+    .then((categories) => {
+      categories.forEach((category) => {
+        if (category._id.toString() === categorySelectId) {
+          categorySelectName = category.name
+        }
       })
-      res.render('index', { records })
+      if (categorySelectName === '選擇類別') {
+        Record.find({ userId })
+          .populate('categoryId')
+          .lean()
+          .then((records) => {
+            let totalAmount = 0
+            records.forEach((record) => {
+              record.date = record.date.toISOString().slice(0, 10)
+              totalAmount += record.amount
+            })
+            res.render('index', {
+              records,
+              totalAmount,
+              categories,
+              categorySelectName,
+            })
+          })
+      } else {
+        Record.find({ userId, categoryId: categorySelectId })
+          .populate('categoryId')
+          .lean()
+          .then((records) => {
+            let totalAmount = 0
+            records.forEach((record) => {
+              record.date = record.date.toISOString().slice(0, 10)
+              totalAmount += record.amount
+            })
+            res.render('index', {
+              records,
+              totalAmount,
+              categories,
+              categorySelectName,
+            })
+          })
+      }
     })
     .catch((error) => console.error(error))
 })
